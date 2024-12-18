02244b2f6e5eb94589f1a28541d51ea8f7386a31
Samir Qureshi <qureshisamar351@gmail.com>
2024-12-18 10:42:44 +0530

    request position change

--- app/Http/Controllers/GoogleSheetController.php
+++ app/Http/Controllers/GoogleSheetController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 use Google\Client;
 use Google\Service\Sheets;
 use Firebase\JWT\JWT;
-
-
-
 use Illuminate\Http\Request;
 
 class GoogleSheetController extends Controller
